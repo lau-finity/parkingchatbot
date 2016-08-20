80667111f651d0a0f70a8174e07c9e9198ad13ab
Leo Kim <Leo Kim>
2016-08-20 18:34:00 -0400

    added send api

--- app.js
+++ app.js
@@ -388,6 +388,35 @@ function sendTextMessage(recipientId, messageText) {
   callSendAPI(messageData);
 }
 
+/*
+ * Call the Send API. The message data goes in the body. If successful, we'll 
+ * get the message id in a response 
+ *
+ */
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s", 
+        recipientId);
+      }
+    } else {
+      console.error(response.error);
+    }
+  });  
+}
 
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid 
